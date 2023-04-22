a4ea2bb0fbc0bd227c77850e8adf28f6f2bbc3fe
Halfarn <halfarn@gmail.com>
2023-04-22 05:20:13 +0100

    added return

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,9 @@
 export default function Header() {
     return(
         <nav>
-            <img src="./assets/globe.png" className="nav--globe"></img><div className="nav--items">my travel journal.</div>
+        <a href="https://www.halfarn.com" className="header--return">← Back to Halfarn.com</a>
+        <div className="header--nav"><img src="./assets/globe.png" className="nav--globe"></img><span className="nav--items">my travel journal.</span></div>
         </nav>
+        
     )
 }
